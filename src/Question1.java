17a6fee6820f792cfd94982732d12b8a65eee66d
PC <PC@com>
2019-11-24 05:26:17 +0530

    TestCommit

--- src/Question1.java
+++ src/Question1.java
@@ -14,6 +14,7 @@ import java.util.TreeSet;
 
 class Question1 {
     public static void enterValues() throws Exception {
+    	
 
         //BufferedReaders
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
